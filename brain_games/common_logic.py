55e2b784e9eb01d99656c8243405e7e1727d6563
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-04-02 18:26:14 +0600

    refactor: move the general logic of games to a common_logic.py

---
+++ brain_games/common_logic.py
@@ -0,0 +1,22 @@
+import random, prompt
+
+
+def logic(condition, get_question, get_answer):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello', name, '!')
+    print(condition)
+    count = 0
+    while count <= 2:
+        question = get_question()
+        print('Question:', question)
+        user_answer = prompt.string('Your answer: ') 
+        real_answer = get_answer(question)
+        if user_answer == real_answer:
+            print('Correct!')
+            count += 1
+        else:
+            result = f'"{user_answer}" is wrong answer ;(. Correct answer was "{real_answer}".'
+            print(result)
+            print("Let's try again,", name, '!')
+    print('Congratulations,', name, '!!')
--- brain_games/even_logic.py
+++ brain_games/even_logic.py
@@ -1,22 +1,15 @@
-import random, prompt
-
+import random
+from . import common_logic
 
 def even():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello', name, '!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    count = 0
-    while count <= 2:
+    condition = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+    def get_number():
         number = random.randint(1, 1000)
-        print('Question:', number)
-        user_answer = prompt.string('Your answer: ')
-        real_answer = 'yes' if number % 2 == 0 else 'no'
-        if user_answer == real_answer:
-            print('Correct!')
-            count += 1
-        else:
-            result = f'"{user_answer}" is wrong answer ;(. Correct answer was "{real_answer}".'
-            print(result)
-            print("Let's try again,", name, '!')
-    print('Congratulations,', name, '!!')
+        return number
+
+    def get_answer(number):
+        real_answer = 'yes' if int(number) % 2 == 0 else 'no'
+        return real_answer
+
+    common_logic.logic(condition, get_number, get_answer)
