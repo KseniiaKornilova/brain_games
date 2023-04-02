44ddfb31bd040ae93f3c6bb8adef32189152fb4f
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-04-02 21:39:07 +0600

    feat: add progression game

---
+++ brain_games/games/progression_logic.py
@@ -0,0 +1,26 @@
+import random
+from .. import common_logic
+
+def progression():
+    condition = 'What number is missing in the progression?'
+
+    def get_progression():
+        initial_number = random.randint(-100, 100)
+        difference = random.randint(1, 10)
+        sequence = [initial_number + i * difference for i in range(10)]
+        gap_index = random.randint(0, len(sequence)-1)
+        global gap 
+        gap = sequence[gap_index] 
+        sequence[gap_index] = '..'
+        sequence = [str(x) for x in sequence]
+        progression = ' '.join(sequence)
+        return progression
+
+
+    def get_answer(progression):
+        return str(gap)
+        
+
+        
+
+    common_logic.logic(condition, get_progression, get_answer)
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from ..games import progression_logic
+
+def main():
+    progression_logic.progression()
+    
+    
+if __name__ == '__main__':
+    main()
