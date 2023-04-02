3a4127401c5a770fe3a9ffa11651ccd12c28426f
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-04-02 19:04:21 +0600

    feat: add gcd game

---
+++ brain_games/games/gcd_logic.py
@@ -0,0 +1,18 @@
+import math, random
+from .. import common_logic
+
+def gcd_calculation():
+    condition = 'Find the greatest common divisor of given numbers.'
+
+    def get_expression():
+        first_number = random.randint(1, 100)
+        second_number = random.randint(1, 100)
+        result = f'{first_number} {second_number}'
+        return result
+
+    def get_answer(expression):
+        first, second = expression.split()
+        real_answer = str(math.gcd(int(first), int(second)))
+        return real_answer
+
+    common_logic.logic(condition, get_expression, get_answer)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from ..games import gcd_logic
+
+def main():
+    gcd_logic.gcd_calculation()
+    
+    
+if __name__ == '__main__':
+    main()
