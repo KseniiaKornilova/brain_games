04417574403d08987ddf1f2c58c2a6c4d630e142
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-04-02 18:47:45 +0600

    feat: add calculation game

---
+++ brain_games/games/calc_logic.py
@@ -0,0 +1,18 @@
+import random
+from .. import common_logic
+
+def calculation():
+    condition = 'What is the result of the expression?'
+
+    def get_expression():
+        first_number = random.randint(1, 1000)
+        second_number = random.randint(1, 1000)
+        operant = random.choice(['+', '-', '*'])
+        result = f'{first_number}{operant}{second_number}'
+        return result
+
+    def get_answer(expression):
+        real_answer = str(eval(expression))
+        return real_answer
+
+    common_logic.logic(condition, get_expression, get_answer)
