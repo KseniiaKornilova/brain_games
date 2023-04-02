db2e57be2512fd59114938f1d01bc7e36bcf2612
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2023-04-02 21:55:49 +0600

    feat: add game about prime number

---
+++ brain_games/games/prime_logic.py
@@ -0,0 +1,21 @@
+import random
+from .. import common_logic
+
+def prime():
+    condition = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+    def get_number():
+        number = random.randint(1, 1000)
+        return number
+
+    def is_prime_number(number):
+        number = int(number)
+        counter = 0
+        for i in range(2, number // 2 + 1):
+            if number % i == 0:
+                counter += 1
+                break
+        result = 'yes' if counter == 0 else 'no'
+        return result
+
+    common_logic.logic(condition, get_number, is_prime_number)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from ..games import prime_logic
+
+def main():
+    prime_logic.prime()
+    
+    
+if __name__ == '__main__':
+    main()
